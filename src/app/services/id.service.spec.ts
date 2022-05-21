5fcb21237e406e2d3821f90731d45199e00df5fb
RubenW556 <Ruubminer@gmail.com>
2022-05-21 23:28:44 +0200

    added testing

---
+++ src/app/services/id.service.spec.ts
@@ -0,0 +1,44 @@
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+
+import {idService } from './id.service';
+import {Router} from "@angular/router"
+import {RouterModule} from "@angular/router";
+
+describe('IdService', () => {
+  let service: idService;
+  let id:Number = 123123;
+  let router:Router;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([])
+      ]
+    });
+    service = TestBed.inject(idService);
+    router = TestBed.inject(Router);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("test for id",
+    fakeAsync( () => {
+      let idFromRoute:String ="";
+
+      router.navigateByUrl('/?id='+id);
+      tick();
+
+      service.getId().then((string) => {
+        idFromRoute = string;
+      });
+      tick();
+
+      expect(idFromRoute).toEqual(id.toString());
+    }
+    )
+
+  )
+
+})
