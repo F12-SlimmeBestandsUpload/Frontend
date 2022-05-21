763a8173f06caae5268c38fdd1c3b0d80d2cb3df
FlodekNL <50317497+FlodekNL@users.noreply.github.com>
2022-05-21 16:32:22 +0200

    Eerste push:
    - Camera word geopend.
    - Camera kan een snapshot maken die eronder word geload.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
+import {WebcamModule} from "ngx-webcam";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebcamModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
