25e6c07e8b2fb25c03a0a9845cfe536af5f77df4
RubenW556 <Ruubminer@gmail.com>
2022-05-22 19:16:50 +0200

    added for loop for image preview creation, currently using numbers to mock the existence of images. Added a delete function initiated from child component and execute via an emitted event

--- src/app/overview/image-overview/image-element/image-element.component.ts
+++ src/app/overview/image-overview/image-element/image-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-image-element',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-element.component.css']
 })
 export class ImageElementComponent implements OnInit {
-
+  @Input() public image:number = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Output()
+  event = new EventEmitter<number>();
+
+  deleteImage() {
+    this.event.emit(this.image);
   }
 
 }
--- src/app/overview/image-overview/image-overview.component.ts
+++ src/app/overview/image-overview/image-overview.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-overview.component.css']
 })
 export class ImageOverviewComponent implements OnInit {
+  public images:number[] = [1,2,3,4,5];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeImage(image: number){
+    this.images = this.images.filter(item => item !== image);
   }
 
 }
