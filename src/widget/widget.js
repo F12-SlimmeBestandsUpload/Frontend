9ef4a89accaefce62f66abd88ee1b25f10f73af9
RubenW556 <Ruubminer@gmail.com>
2022-06-10 13:34:08 +0200

    added gallery functionality and cleaned up code a bit

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -1,60 +1,37 @@
 let override= null;
-let key = null;
-let connection_state = 0; //receiving QR=0, receiving bitmap=1
+let connection_state = 0; //receiving QR=0, receiving imageInfo=1
 
+let blobs = [];
+let selectedBlob;
 
 
 const socket = new WebSocket('ws://'+getIpOfServer()+':6969');
 socket.addEventListener('message', async function (event) {
 
-  if(connection_state == 0) {
-    let img = document.getElementById("result");
+  if (connection_state == 0) {
+    let img = document.getElementById("QR-code");
     img.src = event.data;
-    connection_state= 1;
+    connection_state = 1;
     return
   }
 
-  let Json = JSON.parse(event.data);
-  console.log(Json)
-  let iv = JSON.parse(Json.iv)
-  iv = Uint8Array.from(iv);
-  console.log(iv);
-  let key = JSON.parse(Json.key)
-  console.log(key);
+  let [key, iv, references] = await parseJson(event.data);
 
-  key = await importSecretKey(key)
-  console.log(key)
-  let reference = Json["references"][0]
+  for (const reference of references) {
+    getReference(reference, async function (blob) {
 
-
-
-   getReference(reference, async function(blob){
-     // let img = document.getElementById("tex");
-     // img.src = "https://media.istockphoto.com/vectors/legal-document-vector-id166011405";
-      //console.log(str2ab(blob))
-     console.log(blob);
       blob = await blob.arrayBuffer();
-      console.log(blob);
-      try{
-        blob = await window.crypto.subtle.decrypt({
-          name: "AES-GCM",
-          iv: iv
-        }, key, blob)
-      } catch(error){
-        console.log(error)
-        console.log("jex");
-      }
-      /*let reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onloadend = function() {
-        let base64data = reader.result;
-        let img = document.getElementById("tex");
-        img.src = URL.createObjectUrl(new Blob([blob]));
-      }*/
-     let img = document.getElementById("tex");
-     img.src = URL.createObjectURL(new Blob([blob]));
 
-   });
+      let decryptedBlob = await decrypt(blob, key, iv)
+
+      addImage(decryptedBlob)
+
+      //initialize first image
+      if( selectedBlob === undefined){
+        changeSelection(0)
+     }
+    });
+  }
 })
 
 function getIpOfServer(){
@@ -65,26 +42,18 @@ function getIpOfServer(){
   return ipPlusRoute;
 }
 
-function getReference(url, callback) {
-  let xhr = new XMLHttpRequest();
-  let ip = "http://" + getIpOfServer() + ":8000";
-  xhr.open('get', ip + "/ttl-reference?ref=" + url);
-  xhr.responseType = "blob";
-  xhr.overrideMimeType('binary');
-  xhr.onload = function () {
-    console.log(this.response);
-    callback(this.response);
-  };
-  xhr.send();
-}
-
-function str2ab(str) {
-  const buff = new ArrayBuffer(str.length);
-  const buffView = new Uint8Array(buff);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    buffView[i] = str.charCodeAt(i);
-  }
-  return buff;
+async function parseJson(data){
+  //parse as Json
+  let Json = JSON.parse(data);
+  //parse the IV
+  let iv = JSON.parse(Json.iv)
+  iv = Uint8Array.from(iv);
+  //parse the key
+  let key = JSON.parse(Json.key)
+  key = await importSecretKey(key)
+  //get references
+  let references = Json["references"]
+  return [key, iv, references]
 }
 
 async function importSecretKey(jwk) {
@@ -100,4 +69,60 @@ async function importSecretKey(jwk) {
       "decrypt"
     ]
   );
+}
+
+function getReference(url, callback) {
+  let xhr = new XMLHttpRequest();
+  let ip = "http://" + getIpOfServer() + ":8000";
+  xhr.open('get', ip + "/ttl-reference?ref=" + url);
+  xhr.responseType = "blob";
+  xhr.overrideMimeType('binary');
+  xhr.onload = function () {
+    callback(this.response);
+  };
+  xhr.send();
+}
+
+async function decrypt(blob, key, iv){
+  return await window.crypto.subtle.decrypt({
+    name: "AES-GCM",
+    iv: iv
+  }, key, blob)
+}
+
+
+// image selection
+function goLeft(){
+  if(selectedBlob=== undefined){
+    return
+  }
+  if(selectedBlob == 0){
+    changeSelection(blobs.length-1)
+  }
+  else{
+    changeSelection(selectedBlob-1)
+  }
+}
+
+function goRight(){
+  if(selectedBlob=== undefined){
+    return
+  }
+  if(selectedBlob == blobs.length-1){
+    changeSelection(0)
+  }
+  else{
+
+    changeSelection(selectedBlob+1)
+  }
+}
+
+function changeSelection(index){
+  selectedBlob = index;
+  let img = document.getElementById("tex");
+  img.src = URL.createObjectURL(new Blob([blobs[index]]));
+}
+
+function addImage(blob){
+  blobs.push(blob)
 }
